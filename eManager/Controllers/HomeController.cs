46b795015e7cf4f96af0670c6de8af4e72697e8c
enrique.salazar <enriquesa7@gmail.com>
2017-10-17 01:22:09 -0500

    last

--- eManager/Controllers/HomeController.cs
+++ eManager/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eManager.Domain;
+using eManager.Infraestructure;
 
 namespace eManager.Controllers
 {
     public class HomeController : Controller
     {
+        private IDepartmentDataSource _ds = new DeparmentDb();
+        public HomeController()
+        {
+            
+        }
         public ActionResult Index()
         {
+            var allDepartments=_ds.Deparments;
             ViewBag.test = "asd";
-            return View();
+            return View(allDepartments);
         }
 
         public ActionResult About()
