15300f6a0b34f1f418f4b90ead8b6199cb82081a
enrique.salazar <enriquesa7@gmail.com>
2017-10-16 23:27:06 -0500

    Add project files.

---
+++ eManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eManager.MvcApplication" Language="C#" %>
---
+++ eManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eManager.Startup))]
+namespace eManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
